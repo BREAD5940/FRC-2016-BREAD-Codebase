641ca330136444620426479c88e142e58d2d7093
David Boles <david.e.boles22@gmail.com>
2016-01-16 13:50:56 -0800

    Added to MotorGroup methods.

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
@@ -1,6 +1,22 @@
 package org.usfirst.frc.team5940.motorcontrol;
 
 public interface MotorGroup {
-
 	
+	public void setValue();
+	
+	public float getSetValue();
+	
+	public float getSpeed();
+	
+	public int getGearsAmount();
+	
+	public void getGear();
+	
+	public void setGear();
+	
+	public int getCurrentGear();
+	
+	public void setEnabled();
+	
+	public boolean getEnabled();
 }
