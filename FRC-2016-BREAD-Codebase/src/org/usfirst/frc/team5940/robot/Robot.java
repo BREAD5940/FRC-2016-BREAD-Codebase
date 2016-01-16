34b8d8ee81e54fc0216081a20daccaeb2cb0707b
David Boles <david.e.boles22@gmail.com>
2016-01-16 11:43:13 -0800

    test

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/robot/Robot.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/robot/Robot.java
@@ -4,22 +4,6 @@ package org.usfirst.frc.team5940.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
 public class Robot extends SampleRobot {
 
 	public Robot() {
