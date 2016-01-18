27a88cb6b1fb31d8402ff9f670dedeaae03987ce
David Boles <david.e.boles22@gmail.com>
2016-01-18 11:41:22 -0800

    update

---
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/DualMGDrivetrain.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team5940.motorcontrol;
+
+public class DualMGDrivetrain {
+	
+	MotorGroup left;
+	MotorGroup right;
+	int gears;
+	
+	public DualMGDrivetrain(MotorGroup left, MotorGroup right) {
+		this.left = left;
+		this.right = right;
+		
+		
+	}
+}
--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
@@ -3,8 +3,14 @@ package org.usfirst.frc.team5940.motorcontrol;
 public interface MotorGroup {
 	
 	/**
-	 * Sets the value of output for the motors.
-	 * @param value A float between -1 an 1
+	 * This should return the name of the MotorGroup. This should only be used for human identification purposes, not in code. If no name has been set (for instance through a constructor) it should return the name of its class file.
+	 * @return A String representing the name of the MotorGroup.
+	 */
+	public String getName();
+	
+	/**
+	 * Sets the value of output for the motors. -1 should represent full speed backwards, 0 should represent 
+	 * @param value A float between -1 an 1.
 	 */
 	public void setValue(float value);
 	
