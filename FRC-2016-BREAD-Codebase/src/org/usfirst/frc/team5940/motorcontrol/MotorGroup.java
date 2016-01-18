940a84b1000414a2d6027104a7229e19e0a72a4b
David Boles <david.e.boles22@gmail.com>
2016-01-18 13:39:26 -0800

    UPDT

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/motorcontrol/MotorGroup.java
@@ -9,21 +9,33 @@ public interface MotorGroup {
 	public String getName();
 	
 	/**
-	 * Sets the value of output for the motors. -1 should represent full speed backwards, 0 should represent 
+	 * Sets the value of output for the motors. -1 should represent full speed backwards, 0 should represent completely stopped, 1 should represent full speed forward. Any non linearity or other control systems (voltage based, PID, PWM, etc...) are up to the implementation, as long as they follow those guidelines.
 	 * @param value A float between -1 an 1.
 	 */
 	public void setValue(float value);
 	
 	/**
-	 * 
-	 * @return
+	 * This should return the value corresponding to the last setValue. If the MotorGroup is disabled, it should still return the value that will resume being used when re-enabled.
+	 * @return The float the MotorGroup is set to.
 	 */
 	public float getSetValue();
 	
+	/**
+	 * Returns the speed in rpm of the motor shafts, undefined if no encoder.
+	 * @return A float representing the speed of the motor group in rpm
+	 */
 	public float getSpeed();
 	
+	/**
+	 * Returns the number of gears this MotorGroup has. This should be a number between 1 and infinity as you always have one gear.
+	 * @return An int representing the number of gearing positions this motor group can be in.
+	 */
 	public int getGearsAmount();
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getGear();
 	
 	public void setGear(int gear);
