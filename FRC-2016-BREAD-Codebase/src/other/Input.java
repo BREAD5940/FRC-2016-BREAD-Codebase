b6e69fe0f2769ef66fbc94d3d03a33f10f13e8ff
legodude17 <joshuadb@me.com>
2016-01-21 13:58:33 -0800

    Commented Input.java

--- FRC-2016-BREAD-Codebase/src/other/Input.java
+++ FRC-2016-BREAD-Codebase/src/other/Input.java
@@ -1,7 +1,11 @@
 package other;
 
 public class Input {
+	/**
+	 * Takes an input and power and raises the
+	 * input to the power perserving the negative.
+	*/
 	public double powInputFixed( double input, double power) {
-		return (Math.pow(Math.abs(input),power)* ((input < 0) ? -1:1)
+		return (/*Power*/Math.pow(/*Make input not negetive*/Math.abs(input),power) * (/*If input is negative, make the whole thing negative*/(input < 0) ? -1:1)
 	}
 }
