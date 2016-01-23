640ccc71382135c2c997ddcea3ef98dea01e3740
Michael Bentley <mbentley124@gmail.com>
2016-01-22 19:44:37 -0800

    hi

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/opcon/OpConStandardState.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/opcon/OpConStandardState.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc.team5940.states.opcon;
 import org.usfirst.frc.team5940.motorcontrol.CANTalonSimpleGroup;
 import org.usfirst.frc.team5940.motorcontrol.DualMGDrivetrain;
+import org.usfirst.frc.team5940.motorcontrol.VictorSimpleGroup;
 import org.usfirst.frc.team5940.states.State;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Victor;
 import other.Input;
 
 public class OpConStandardState extends State{
@@ -29,13 +31,13 @@ public class OpConStandardState extends State{
 		controller = new Joystick (0);
 		//driver = new DualMGDrivetrain(new CANTalonDrive(1,2),new CANTalonDrive(3,4));
 		// TODO make the variables not equal to null
-		CANTalon r1 = new CANTalon(1);
-		CANTalon r2 = new CANTalon(2);
-		CANTalon l3 = new CANTalon(3);
-		CANTalon l4 = new CANTalon(4);
+		Victor r1 = new Victor(1);
+		Victor r2 = new Victor(2);
+		Victor l3 = new Victor(3);
+		Victor l4 = new Victor(4);
 		
 		//TODO incorrect infersions here and below...
-		driveThing =  new DualMGDrivetrain(new CANTalonSimpleGroup(new CANTalon[]{l3, l4}, false), new CANTalonSimpleGroup(new CANTalon[]{r1, r2}, true));
+		driveThing =  new DualMGDrivetrain(new VictorSimpleGroup(new Victor[]{l3, l4}, false), new VictorSimpleGroup(new Victor[]{r1, r2}, true));
 	}
 
 	@Override
