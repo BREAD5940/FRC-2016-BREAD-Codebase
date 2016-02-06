cfeee697d2464f12b073cc25bb2ac68f104ead60
bertturtle <mbentley124@gmail.com>
2016-02-05 17:01:14 -0800

    hi

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
@@ -1,11 +1,21 @@
 package org.usfirst.frc.team5940.states.auto;
 
+import org.usfirst.frc.team5940.motorcontrol.DualMGDrivetrain;
+import org.usfirst.frc.team5940.motorcontrol.DualMGShiftingDrivetrain;
+import org.usfirst.frc.team5940.motorcontrol.VictorSimpleGroup;
 import org.usfirst.frc.team5940.states.State;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Victor;
 
 public class AutoStandardState extends State{
-
+	
+	DualMGDrivetrain driver;
+	Encoder leftEncoder;
+	Encoder rightEncoder;
+	
 	public AutoStandardState(RobotBase robot) {
 		//Call the state constructor
 		super(robot);
@@ -14,13 +24,26 @@ public class AutoStandardState extends State{
 	@Override
 	protected void init() {
 		// TODO Make the init
-		
+		rightEncoder = new Encoder(0, 1);
+		leftEncoder = new Encoder(2, 3);
+		Victor r1 = new Victor(1);
+		Victor r2 = new Victor(3);
+		Victor l3 = new Victor(0);
+		Victor l4 = new Victor(2);
+		driver = new DualMGShiftingDrivetrain(new VictorSimpleGroup(new Victor[] { l3, l4 }, false),
+				new VictorSimpleGroup(new Victor[] { r1, r2 }, true), new DoubleSolenoid(0, 1), 1);
 	}
 
 	@Override
 	protected void update() {
 		// TODO Make the auto
-		
-	}
-
+		while (leftEncoder.getDistance() < 400) {
+			driver.moveStraight((float) 0.75, (float) leftEncoder.getRate(), (float) rightEncoder.getRate());
+		}
+		leftEncoder.reset();
+		rightEncoder.reset();
+		while (leftEncoder.getDistance() < 400) {
+			driver.moveStraight((float) -0.75, (float) leftEncoder.getRate(), (float) rightEncoder.getRate());
+		}
+		}
 }
