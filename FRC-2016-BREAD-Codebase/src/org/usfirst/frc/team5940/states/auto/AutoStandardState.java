8f38e030f5c2ff4aac599a846e0eff6cc5773622
bertturtle <mbentley124@gmail.com>
2016-02-10 20:27:30 -0800

    hi

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
@@ -8,6 +8,7 @@ import org.usfirst.frc.team5940.states.State;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -33,49 +34,60 @@ public class AutoStandardState extends State {
 		Victor l4 = new Victor(2);
 		int auto_program = 1;
 		int auto_distance_of_1 = 400;
-		
+		Ultrasonic sensor = new Ultrasonic(1, 2);
+
 		driver = new DualMGShiftingDrivetrain(new VictorSimpleGroup(new Victor[] { l3, l4 }, false),
 				new VictorSimpleGroup(new Victor[] { r1, r2 }, true), new DoubleSolenoid(0, 1), 1);
-		// while (leftEncoder.getDistance() < 400) {
-		// driver.moveStraight((float) 0.75, (float) leftEncoder.getRate(),
-		// (float) rightEncoder.getRate());
-		// }
-		// leftEncoder.reset();
-		// rightEncoder.reset();
-		// while (leftEncoder.getDistance() < 400) {
-		// driver.moveStraight((float) -0.75, (float) leftEncoder.getRate(),
-		// (float) rightEncoder.getRate());
-		// }
-		
-		//back-and-forth auto program
-		if (auto_program == 1){
-		leftEncoder.setDistancePerPulse(1);
-		rightEncoder.setDistancePerPulse(1);
-		while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
-			driver.updateArcade(0.5, 0.5, 1);
-			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
-			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
-			
-		}
-		driver.updateArcade(0, 0, 1);
-		leftEncoder.reset();
-		rightEncoder.reset();
-		while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
-			driver.updateArcade(-0.5, -0.5, 1);
-			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
-			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
-			
-		}
-		driver.updateArcade(0, 0, 1);
-		leftEncoder.reset();
-		rightEncoder.reset();
+				// while (leftEncoder.getDistance() < 400) {
+				// driver.moveStraight((float) 0.75, (float)
+				// leftEncoder.getRate(),
+				// (float) rightEncoder.getRate());
+				// }
+				// leftEncoder.reset();
+				// rightEncoder.reset();
+				// while (leftEncoder.getDistance() < 400) {
+				// driver.moveStraight((float) -0.75, (float)
+				// leftEncoder.getRate(),
+				// (float) rightEncoder.getRate());
+				// }
 
+		// back-and-forth auto program
+		if (auto_program == 1) {
+			leftEncoder.setDistancePerPulse(1);
+			rightEncoder.setDistancePerPulse(1);
+			while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
+				driver.updateArcade(0.5, 0.5, 1);
+				SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
+				SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
+
+			}
+			driver.updateArcade(0, 0, 1);
+			leftEncoder.reset();
+			rightEncoder.reset();
+			while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
+				driver.updateArcade(-0.5, -0.5, 1);
+				SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
+				SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
+
+			}
+			driver.updateArcade(0, 0, 1);
+			leftEncoder.reset();
+			rightEncoder.reset();
+
+		}
+		else if (auto_program == 2) {
+			while (sensor.getRangeInches() < 20) {
+				driver.updateArcade(0.5, 0, 1);
+				SmartDashboard.putNumber("Distance From Objects", sensor.getRangeInches());
+			}
+			driver.updateArcade(0, 0, 0);
 		}
 	}
 
 	@Override
 	protected void update() {
-		// TODO Make the auto Note: now that I'm looking at it, this should be in INIT()
+		// TODO Make the auto Note: now that I'm looking at it, this should be
+		// in INIT()
 
 	}
 }
