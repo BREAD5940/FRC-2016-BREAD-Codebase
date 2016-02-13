bd869099cea1764c46ebbc8c545469efd8f30b41
Michael Bentley <mbentley124@gmail.com>
2016-02-12 20:20:49 -0800

    hi

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoStandardState.java
@@ -17,14 +17,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoStandardState extends State {
 
-	//DualMGDrivetrain driver;
+	// DualMGDrivetrain driver;
 	Encoder leftEncoder = new Encoder(2, 3);
 	Encoder rightEncoder = new Encoder(0, 1);
 	Victor r1 = new Victor(1);
 	Victor r2 = new Victor(3);
 	Victor l3 = new Victor(0);
 	Victor l4 = new Victor(2);
-	Ultrasonic sensor = new Ultrasonic(1, 2);
+	Ultrasonic leftUltrasonic = new Ultrasonic(1, 2);
+	Ultrasonic rightUltrasonic = new Ultrasonic(3, 4);
 
 	int auto_program = 1;
 	int auto_distance_of_1 = 400; // TODO Get the right value
@@ -37,8 +38,10 @@ public class AutoStandardState extends State {
 									// smaller than "auto_distance_of_4a"
 									// than you measured VERY wrong )
 	int auto_distance_of_5 = 400;
-//	rightEncoder.setDistancePerPulse(1);
-//	leftEncoder.setDistancePerPulse(1);
+	int auto_distance_of_6 = 500;
+	int distanceToBackOfHole;
+	// rightEncoder.setDistancePerPulse(1);
+	// leftEncoder.setDistancePerPulse(1);
 
 	DualMGDrivetrain driver = new DualMGShiftingDrivetrain(new VictorSimpleGroup(new Victor[] { l3, l4 }, false),
 			new VictorSimpleGroup(new Victor[] { r1, r2 }, true), new DoubleSolenoid(0, 1), 1);
@@ -53,7 +56,7 @@ public class AutoStandardState extends State {
 		super(robot);
 	}
 
-	public void back_and_forth() {
+	public void backAndForth() {
 		while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
@@ -75,18 +78,18 @@ public class AutoStandardState extends State {
 
 	}
 
-	public void ultrasonic_home_in() {
-		while (sensor.getRangeInches() < 20) {
+	public void ultrasonicHomeIn() {
+		while (leftUltrasonic.getRangeInches() > 20) {
 			driver.updateArcade(0.5, 0, 1);
-			SmartDashboard.putNumber("Distance From Objects", sensor.getRangeInches());
+			SmartDashboard.putNumber("Distance From Objects", leftUltrasonic.getRangeInches());
 		}
 		driver.updateArcade(0, 0, 0);
 	}
 
 	// Putting ball in courtyard if you already have the ball and going
 	// through
-	
-	public void breach_and_pass () {
+
+	public void breachAndPass() {
 		while (leftEncoder.getDistance() < auto_distance_of_3 && rightEncoder.getDistance() < auto_distance_of_3) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
@@ -119,9 +122,8 @@ public class AutoStandardState extends State {
 
 	// an auto program that goes forward, to a different defense, and goes
 	// through that defense.
-	public void loop_around() {
-		while (leftEncoder.getDistance() < auto_distance_of_4a
-				&& rightEncoder.getDistance() < auto_distance_of_4a) {
+	public void loopAround() {
+		while (leftEncoder.getDistance() < auto_distance_of_4a && rightEncoder.getDistance() < auto_distance_of_4a) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
 			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
@@ -131,8 +133,7 @@ public class AutoStandardState extends State {
 		leftEncoder.reset();
 		rightEncoder.reset();
 
-		while (leftEncoder.getDistance() < auto_distance_of_4b
-				&& rightEncoder.getDistance() < auto_distance_of_4b) {
+		while (leftEncoder.getDistance() < auto_distance_of_4b && rightEncoder.getDistance() < auto_distance_of_4b) {
 			driver.updateArcade(0, 0.5, 1); // TODO find the right direction
 											// and value.
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
@@ -143,8 +144,7 @@ public class AutoStandardState extends State {
 		leftEncoder.reset();
 		rightEncoder.reset();
 
-		while (leftEncoder.getDistance() < auto_distance_of_4c
-				&& rightEncoder.getDistance() < auto_distance_of_4c) {
+		while (leftEncoder.getDistance() < auto_distance_of_4c && rightEncoder.getDistance() < auto_distance_of_4c) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
 			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
@@ -154,8 +154,7 @@ public class AutoStandardState extends State {
 		leftEncoder.reset();
 		rightEncoder.reset();
 
-		while (leftEncoder.getDistance() < auto_distance_of_4b
-				&& rightEncoder.getDistance() < auto_distance_of_4b) {
+		while (leftEncoder.getDistance() < auto_distance_of_4b && rightEncoder.getDistance() < auto_distance_of_4b) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
 			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
@@ -165,8 +164,7 @@ public class AutoStandardState extends State {
 		leftEncoder.reset();
 		rightEncoder.reset();
 
-		while (leftEncoder.getDistance() < auto_distance_of_4a
-				&& rightEncoder.getDistance() < auto_distance_of_4a) {
+		while (leftEncoder.getDistance() < auto_distance_of_4a && rightEncoder.getDistance() < auto_distance_of_4a) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
 			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
@@ -178,8 +176,7 @@ public class AutoStandardState extends State {
 
 	}
 
-	
-	public void move_forward() {
+	public void moveForward() {
 		while (leftEncoder.getDistance() < auto_distance_of_1 && rightEncoder.getDistance() < auto_distance_of_1) {
 			driver.updateArcade(0.5, 0, 1);
 			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
@@ -191,11 +188,63 @@ public class AutoStandardState extends State {
 		rightEncoder.reset();
 
 	}
-	
+
+	public void breachAndShoot() {
+		while (leftEncoder.getDistance() < auto_distance_of_3 && rightEncoder.getDistance() < auto_distance_of_3) {
+			driver.updateArcade(0.5, 0, 1);
+			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
+			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
+
+		}
+		//driver.updateArcade(0, 0, 1);
+		leftEncoder.reset();
+		rightEncoder.reset();
+
+		while (leftEncoder.getDistance() < auto_distance_of_6 && rightEncoder.getDistance() < auto_distance_of_6) {
+			driver.updateArcade(0.5, 0, 1);
+			SmartDashboard.putNumber("Distance of the left encoder", leftEncoder.getDistance());
+			SmartDashboard.putNumber("Distance of the right encoder", rightEncoder.getDistance());
+		}
+		driver.updateArcade(0, 0, 1);
+		leftEncoder.reset();
+		rightEncoder.reset();
+
+		driver.spinToAngle(30, true);
+		// Both need to be greater than 10 inches
+		// Both need to be less than 35 inches
+		// Both need to be within 20 inches of each other
+		if (leftUltrasonic.getRangeInches() < 6 || leftUltrasonic.getRangeInches() > 35
+				|| rightUltrasonic.getRangeInches() - leftUltrasonic.getRangeInches() > 20) {
+			// turn right
+			while (leftUltrasonic.getRangeInches() < 6 || leftUltrasonic.getRangeInches() > 35
+					|| rightUltrasonic.getRangeInches() - leftUltrasonic.getRangeInches() > 20) {
+				driver.updateArcade(0, 0.25, 1);
+			}
+		} else if (rightUltrasonic.getRangeInches() < 6 || rightUltrasonic.getRangeInches() > 35
+				|| leftUltrasonic.getRangeInches() - rightUltrasonic.getRangeInches() > 20) {
+			// turn left
+			while (rightUltrasonic.getRangeInches() < 6 || rightUltrasonic.getRangeInches() > 35
+					|| leftUltrasonic.getRangeInches() - rightUltrasonic.getRangeInches() > 20) {
+				driver.updateArcade(0, -0.25, 1);
+			}
+		}
+		driver.updateArcade(0, 0, 1);
+		leftEncoder.reset();
+		rightEncoder.reset();
+		shooter.setValue((float) 1);
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		shooter.setValue(0);
+	}
+
 	@Override
 	protected void init() {
 		// TODO Make the init
-		
+		ultrasonicHomeIn();
 
 		// driver = new DualMGShiftingDrivetrain(new VictorSimpleGroup(new
 		// Victor[] { l3, l4 }, false),
@@ -213,10 +262,7 @@ public class AutoStandardState extends State {
 		// (float) rightEncoder.getRate());
 		// }
 
-		
-
 		// back-and-forth auto program
-		
 
 	}
 
