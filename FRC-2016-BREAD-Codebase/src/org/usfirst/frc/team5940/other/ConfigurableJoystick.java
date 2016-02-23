c6d8577dc036f671b1128aeee94a5ee5ec1cfb30
David Boles <david.e.boles22@gmail.com>
2016-02-22 20:18:07 -0800

    Commented ConfigurableJoystick

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/other/ConfigurableJoystick.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/other/ConfigurableJoystick.java
@@ -7,12 +7,21 @@ public class ConfigurableJoystick extends Joystick {
 	double[][] deadzones;
 	double[] exponents;
 	
+	/**
+	 * Constructor
+	 * @param port The port of the joystick, passed to super.
+	 * @param deadzones A 2d array of the deadzones of the joystick. The first dimension is for each axis of the joystick. The second is two doubles as the lower, then upper bound of the deadzone.
+	 * @param exponents A 2d array of exponents to raise the input to per axis. Negative inputs are corrected for, no need to worry.
+	 */
 	public ConfigurableJoystick(int port, double[][] deadzones, double[] exponents) {
 		super(port);
 		this.deadzones = deadzones;
 		this.exponents = exponents;
 	}
 	
+	/**
+	 * Overrides getRawAxis of super. Applies deadzones and exponents.
+	 */
 	@Override
 	public double getRawAxis(final int axis) {
 		double out = super.getRawAxis(axis);
@@ -21,6 +30,12 @@ public class ConfigurableJoystick extends Joystick {
 		return out;
 	}
 	
+	/**
+	 * Local method to add the deadzones and scale remaining.
+	 * @param axis The axis of the joystick
+	 * @param value The currect computed value of said joystick
+	 * @return The new value of the stick
+	 */
 	double addDeadzone(int axis, double value) {
 		double lowDead = deadzones[axis][0];
 		double highDead = deadzones[axis][1];
@@ -37,7 +52,7 @@ public class ConfigurableJoystick extends Joystick {
 	/**Puts something to a power while saving the negative
 	 * @param value The input
 	 * @param power Power to put it to
-	 * @return result THe result
+	 * @return result The result
 	*/
 	double addPowInputFixed(int axis, double value) {
 		double power = exponents[axis];
