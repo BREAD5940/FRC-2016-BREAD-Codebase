43cb465de729f0138cbe80b0b8866e8ab2324eac
David Boles <david.e.boles22@gmail.com>
2016-02-22 19:44:44 -0800

    Commented State.

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/State.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/State.java
@@ -3,17 +3,23 @@ package org.usfirst.frc.team5940.states;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public abstract class State implements Runnable {
-	//Unused things
 	@SuppressWarnings("unused")
 	protected RobotBase robot;
-	//Refresh delay
+	//Update recall delay
 	private int delay = 25;
 	
+	/**
+	 * Constructor
+	 * @param robot Saved in this class, allows states to determine information about the robot. This should be passed the subclass of RobotBase of your code.
+	 */
 	public State (RobotBase robot){
 		//Set the robot
 		this.robot = robot;
 	}
 	
+	/**
+	 * Overriden run method for Runnable superclass. Started with STATE_RUNNABLE_NAME.run(); or STATE_THREAD_NAME.start();
+	 */
 	@Override
 	public void run() {
 		//Initilize
@@ -28,8 +34,15 @@ public abstract class State implements Runnable {
 			} catch (InterruptedException e) { /*Print the error*/e.printStackTrace(); }
 		}
 	}
-	//Abstract things
+	
+	/**
+	 * Called once right after run() is called, even if the Thread is interrupted.
+	 */
 	protected abstract void init();
+	
+	/**
+	 * Called forever with a delay of delay while the Thread is not interrupted.
+	 */
 	protected abstract void update();
 	
 }
