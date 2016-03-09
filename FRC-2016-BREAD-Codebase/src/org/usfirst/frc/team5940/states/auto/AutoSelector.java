42c525698951152d13d8820915e5c389e3ed0dff
Michael Bentley <mbentley124@gmail.com>
2016-03-08 17:03:17 -0800

    hi

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoSelector.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/auto/AutoSelector.java
@@ -10,10 +10,10 @@ public class AutoSelector {
 	/** This function chooses a auto program based on switch input. 
 	 */
 	public static State getSelectedAuto(RobotBase robot) {
-		if(Components.sel0.get()) return new BreachAndShoot(robot);
+		if(Components.sel0.get()) return null;
 		if(Components.sel1.get()) return new DoubleBreach(robot);
 		if(Components.sel2.get()) return new BreachAndPass(robot);
-		if(Components.sel3.get()) return new ShootFromSpy(robot);
+		if(Components.sel3.get()) return null;
 		if(Components.sel4.get()) return null;
 		if(Components.sel5.get()) return null;
 		return null;
