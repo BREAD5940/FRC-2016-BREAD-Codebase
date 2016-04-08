1b912d4ff47e6aded36ffadf17edba2b36e99086
David Boles <david.e.boles22@gmail.com>
2016-04-07 21:48:46 -0700

    Testing code

--- FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/testing/TestingStandardState.java
+++ FRC-2016-BREAD-Codebase/src/org/usfirst/frc/team5940/states/testing/TestingStandardState.java
@@ -2,8 +2,17 @@ package org.usfirst.frc.team5940.states.testing;
 
 import org.usfirst.frc.team5940.management.Components;
 import org.usfirst.frc.team5940.states.State;
-import org.usfirst.frc.team5940.states.auto.AutoSelector;
+import org.usfirst.frc.team5940.states.testing.tests.AutoProgramsTest;
 import org.usfirst.frc.team5940.states.testing.tests.AutoSelectorTest;
+import org.usfirst.frc.team5940.states.testing.tests.BallSensorTest;
+import org.usfirst.frc.team5940.states.testing.tests.DrivetrainBackwardTest;
+import org.usfirst.frc.team5940.states.testing.tests.DrivetrainForwardTest;
+import org.usfirst.frc.team5940.states.testing.tests.DrivetrainLeftTest;
+import org.usfirst.frc.team5940.states.testing.tests.DrivetrainRightTest;
+import org.usfirst.frc.team5940.states.testing.tests.GearShiftDownTest;
+import org.usfirst.frc.team5940.states.testing.tests.GearShiftUpTest;
+import org.usfirst.frc.team5940.states.testing.tests.RollerInwardTest;
+import org.usfirst.frc.team5940.states.testing.tests.RollerOutwardTest;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -13,7 +22,18 @@ public class TestingStandardState extends State {
 
 	public static final String ALL_TESTS_NAME = "Systems Test";
 	public static final Test[] registeredTests = {
-		new AutoSelectorTest()
+		new AutoProgramsTest(), 
+		new AutoSelectorTest(),
+		new BallSensorTest(),
+		new DrivetrainBackwardTest(),
+		new DrivetrainForwardTest(),
+		new DrivetrainLeftTest(),
+		new DrivetrainRightTest(),
+		new GearShiftDownTest(),
+		new GearShiftUpTest(),
+		new GearShiftDownTest(),
+		new RollerInwardTest(),
+		new RollerOutwardTest()
 	};
 	public static final float rightValue = 0.5f;
 	public static final float leftValue = 0.5f;
